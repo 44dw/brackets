bf7fe7a133c7398b0b17264f2a8f93b4ee85e34d
44dw <elevation1987@gmail.com>
2020-03-08 17:57:13 +0400

    init: Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const isOdd = (num) => num % 2 == 0;
+  let arr = [];
+  const flatConfig = bracketsConfig.flat();
+  for (let bracket of str.split('')) {
+    const index = flatConfig.indexOf(bracket)
+    const isOpen = isOdd(index);
+    const isCloseEqualsToOpen = isOdd(index) && flatConfig[index + 1] == bracket;
+    if (isOpen && !isCloseEqualsToOpen) {
+      arr.push(bracket);
+    } else if (isCloseEqualsToOpen) {
+      if (arr[arr.length - 1] == bracket) {
+        arr.pop();
+      } else {
+        arr.push(bracket);
+      }
+    } else {
+      // close bracket
+      const open = arr.pop();
+      if (open !== flatConfig[index - 1]) return false;
+    }
+  }
+  return  (arr.length == 0) ? true : false;
 }
